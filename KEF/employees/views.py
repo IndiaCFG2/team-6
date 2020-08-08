b99e2cf646148960f0b0fff00aa239be3ae57c1e
unknown <meghana.499@gmail.com>
2020-08-08 16:26:57 +0530

    employee database and users

--- KEF/employees/views.py
+++ KEF/employees/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    emps = Employee.objects.all()
+
+    return render(request, "index.html", {'emps': emps})
