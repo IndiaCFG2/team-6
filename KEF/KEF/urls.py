e99b74ae5903c8f78e4b5f9d7ae8a478486085ab
unknown <meghana.499@gmail.com>
2020-08-08 19:54:49 +0530

    navigation

--- KEF/KEF/urls.py
+++ KEF/KEF/urls.py
@@ -15,17 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 urlpatterns=[ path('', include('schools.urls')),
                 path('',include('employees.urls')),
-                # path('admin/', admin.site.urls),
-                ]
+                path('admin/', admin.site.urls),
+                url(r'^$', TemplateView.as_view(template_name='index.html'),
+name='home'),
+]
+            
+            
+
+# the regex ^$ matches empty
+
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
